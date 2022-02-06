5ef5871d4c1c59bf0dc0ebea38da6a614e3b6476
hodiJavaQa <99143515+hodiJavaQa@users.noreply.github.com>
2022-02-06 21:46:32 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,57 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // this is a comment line
+
+        /*
+        this is a comment block
+        please ignore me
+        im not exist
+         */
+
+
+        //System.out.println("hodi love flafle");
+        //System.out.println(15 * 2);
+
+        // whole number
+        int salary = 5500;
+        short short1 = 3400;
+        long l1 = 16_000_000_000l;
+        byte age = 127;
+
+        //fractional numbers
+        float f1 = 1666.2433F;
+        double d1 = 0.25;
+
+        //boolean
+        boolean b1 = true; //false
+
+        //char
+        char c1 = '+';
+
+
+        System.out.println(15 + 5);//20
+        System.out.println(2 - 5);//-3
+        System.out.println(15 * 5);//75
+        System.out.println(17 / 5);//3
+        System.out.println(23 % 5);//3
+        int x = 4, y = 5;
+        System.out.println("============================");
+        System.out.println(x + y);//9
+        System.out.println(x - y);//-1
+        System.out.println(x * y);//20
+        System.out.println(x / y);//0
+        System.out.println(x % y);//4
+        System.out.println();
+
+        x=x+y;  x+=y;
+        x=x-y;  x-=y;
+        x=x*y;  x*=y;
+        x=x/y;  x/=y;
+        x=x%y;  x%=y;
+
+
+    }
+
+}
+
